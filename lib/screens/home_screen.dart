0b252bb42db2663658a7ca2a33fcdd47549bd3a2
CatapangJay <38466188+CatapangJay@users.noreply.github.com>
2019-12-16 02:59:32 +0800

    Added firestore get functionalities

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,13 +8,66 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   var data;
-  final _firestore = Firestore.instance;
+  final firestore = Firestore.instance;
+  List<DocumentSnapshot> articles = [];
+  bool isLoading = false; // track if products fetching
+  bool hasMore = true; // flag for more products available or not
+  int documentLimit = 10; // documents to be fetched per request
+  DocumentSnapshot lastDocument; // flag for last document from where next 10 records to be fetched
+  ScrollController _scrollController = ScrollController(); // listener for listview scrolling
 
-  void getArticles() async{
-    final articles = await _firestore.collection('articles').getDocuments();
-    for(var article in articles.documents){
+  @override
+  void initState() {
+    _scrollController.addListener(() {
+      double maxScroll = _scrollController.position.maxScrollExtent;
+      double currentScroll = _scrollController.position.pixels;
+      double delta = MediaQuery.of(context).size.height * 0.20;
+      if (maxScroll - currentScroll <= delta) {
+        getArticles();
+      }
+    });
+    super.initState();
+    this.getArticles();
+  }
 
+
+  void getArticles() async {
+    if (!hasMore) {
+      print('No More Products');
+      return;
     }
+    if (isLoading) {
+      return;
+    }
+    setState(() {
+      isLoading = true;
+    });
+
+    QuerySnapshot querySnapshot;
+    if (lastDocument == null) {
+      querySnapshot = await firestore
+          .collection('articles')
+          .limit(documentLimit)
+          .getDocuments();
+    }
+    else {
+      querySnapshot = await firestore
+          .collection('articles')
+          .startAfterDocument(lastDocument)
+          .limit(documentLimit)
+          .getDocuments();
+    }
+
+    if (querySnapshot.documents.length < documentLimit) {
+      hasMore = false;
+    }
+
+    lastDocument = querySnapshot.documents[querySnapshot.documents.length - 1];
+    articles.addAll(querySnapshot.documents);
+
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
@@ -23,22 +76,48 @@ class _HomeScreenState extends State<HomeScreen> {
       resizeToAvoidBottomPadding: false,
       backgroundColor: Colors.grey[200],
       body: Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Column(
-              children: <Widget>[
-                new Icon(Icons.chrome_reader_mode,
-                    color: Colors.grey, size: 60),
-                new Text(
-                  "No articles available",
-                  style: new TextStyle(fontSize: 15.0, color: Colors.grey),
-                )
-              ],
-            )
-          ],
+        child: new Expanded(
+          child: isLoading
+              ? const Center(child: const CircularProgressIndicator())
+              : articles.length != 0
+                ? new ListView.builder(
+                    itemCount: articles.length,
+                    padding: new EdgeInsets.all(8.0),
+                    itemBuilder: (BuildContext context, int index) {
+                      return new Card(
+                        elevation: 1.7,
+                        child: new Padding(
+                          padding: new EdgeInsets.all(10.0),
+                          child: new Column(
+                            children: [
+                              new Row(
+                                children: <Widget>[],
+                              )
+                            ],
+                          )
+                        ),
+                      );
+                    }
+                  ):
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Column(
+                children: <Widget>[
+                  new Icon(Icons.chrome_reader_mode,
+                      color: Colors.grey, size: 60),
+                  new Text(
+                    "No articles available",
+                    style: new TextStyle(fontSize: 15.0, color: Colors.grey),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
